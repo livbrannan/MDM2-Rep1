0de96a59467f09f789d77af02d3bef6f6becb3a0
Liv Brannan <124689883+livbrannan@users.noreply.github.com>
2023-12-06 22:16:51 +0000

    Add files via upload

---
+++ calculate_new_f_0.m
@@ -0,0 +1,5 @@
+function new_f_0 = calculate_new_f_0(approx_grad)
+    % Assuming a linear relationship, you can customize this formula
+    % based on your actual requirements and context.
+    new_f_0 = sum(approx_grad); % Just a simple example, modify accordingly
+end
